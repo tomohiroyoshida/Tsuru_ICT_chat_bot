0962c8e38330c6d553597b7481bc3a31232d4da6
tomo <tomohiroyoshida0511@gmail.com>
2021-03-08 15:14:58 +0900

    add error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,35 +1,55 @@
 <template>
   <v-app id="error">
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
+    <h1 v-if="error.statusCode === 404" class="mt-14">
+      <v-card
+        outlined
+        color="#F5F5F5"
+        height="200"
+        rounded="30"
+        class="px-4 py-8 text-center"
+      >
+        <div class="mb-3">お探しのページが見つかりません。</div>
+        <div class="text-subtitle-1 mb-3">URLが正しいか確認してください。</div>
+        <NuxtLink to="/" class="text-subtitle-1"> ホーム画面へ戻る</NuxtLink>
+      </v-card>
+      <v-img contain max-height="500" src="/cat.webp" />
+      <div class="text-caption text-center">
+        illustrated by
+        <a href="https://shigureni.com/" target="_blank" class="ml-1">
+          shigureni
+        </a>
+      </div>
     </h1>
-    <h1 v-else>
-      {{ otherError }}
+
+    <h1 v-else class="mt-14">
+      <v-card
+        outlined
+        color="#F5F5F5"
+        height="200"
+        rounded="30"
+        class="px-4 py-8 text-center"
+      >
+        <div class="mb-3">エラーが発生しました。</div>
+        <div class="text-subtitle-1 mb-3">ページをリロードしてください。</div>
+        <NuxtLink to="/" class="text-h6"> ホーム画面へ戻る</NuxtLink>
+      </v-card>
+      <v-img contain max-height="500" src="/cat.webp" />
+      <div class="text-caption text-center">
+        illustrated by
+        <a href="https://shigureni.com/" target="_blank" class="ml-1">
+          shigureni
+        </a>
+      </div>
     </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
   </v-app>
 </template>
 
 <script>
 export default {
-  layout: 'empty',
   props: {
     error: {
       type: Object,
       default: null
-    }
-  },
-  data() {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
-    }
-  },
-  head() {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title
     }
   }
 }
